d8fc775c4cb4028478ba1913a2fe6c26402eccf9
J-Cleyton <johnney.cleyton@gmail.com>
2025-01-27 14:10:32 -0300

    Aula 22-01

--- validacao.js
+++ validacao.js
@@ -1,6 +1,6 @@
 import joi from 'joi';
  
-export const modelocarro = joi.object({
+export const modeloCarro = joi.object({
         nome: joi.string().min(3).required(),
         sigla: joi.string().length(3).required(),
         velocidadeMaxima: joi.number().min(1).required(),
